35aa87663da80ab2ab6df08f6f0394fbc562ef30
MD Javedul Ferdous <mdjavedulferdous@gmail.com>
2018-02-12 00:53:31 +0600

    update

--- DGC.py
+++ DGC.py
@@ -57,7 +57,7 @@ classifier.fit_generator(training_set,
                          validation_steps = 2)
 
 # Part 3 - Making new predictions
-test_image = image.load_img('dataset/classifier_prediction/cat_or_dog_3.jpg', target_size = (64, 64))
+test_image = image.load_img('dataset/classifier_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
